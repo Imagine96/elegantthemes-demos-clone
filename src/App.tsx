2c41d8c55f074fd8ce79d3ad4800a55850a7ad85
Ruben Guedes <imagine@macbook-air-de-ruben.home>
2021-12-30 03:26:10 +0100

    banners/fresh-beans

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./componets/hero-section/heroSection";
 import Menu from "./componets/menu-section/menuSection";
 import Video from "./componets/video-section/videoSection";
+import FreshBeansBanner from "./componets/banners/fresh-beans/freshBeansBanner";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Menu />
       <Video />
+      <FreshBeansBanner />
     </main>
   );
 }
---
+++ src/componets/banners/fresh-beans/freshBeansBanner.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ImgLink from "../../img-author/imgLink";
+import classes from "./freshBeansBanner.module.css"
+
+const FreshBeansBanner = () => {
+
+    const byLink = 'https://unsplash.com/@kittinskie?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
+    const onLink = "https://unsplash.com/s/photos/coffe-beans?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    const linkText = "Tina Guina"
+
+    return(
+        <article
+            id="banner-fresh-beans"
+            className=" grid lg:grid-cols-2 "
+        >
+            <div className="p-4 flex flex-col justify-around items-center " >
+                <h1 className="text-[40px] lg:text-[64px] my-4 font-display uppercase " >FRESH BEANS</h1>
+                <p className="text-center" >
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                </p>
+                <button type="button" className="font-semibold text-white py-2 px-4 bg-orange-normal m-8 hover:bg-orange-hover hover:scale-110 transition duration-300" >
+                    LEARN MORE
+                </button>
+            </div>
+            <div className={`${classes.container} invisible lg:visible `} >
+                <ImgLink byLink={byLink} onLink={onLink} text={linkText } />
+            </div>
+        </article>
+    )
+}
+
+export default FreshBeansBanner
